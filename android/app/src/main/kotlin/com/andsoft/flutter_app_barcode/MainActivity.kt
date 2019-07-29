33ed04017445457255e58923167af9509ac8b3a7
Yasnier Perdigón Lorenzo <yasnier1989@gmail.com>
2019-07-29 16:08:12 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/andsoft/flutter_app_barcode/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.andsoft.flutter_app_barcode
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
